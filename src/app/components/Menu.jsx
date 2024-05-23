2f7575c42605dcad0160d3ea995976e934ed7962
Kênia Caroline <keniacarolineferreira@gmail.com>
2024-05-22 22:31:35 -0300

    Ajustes

--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -2,7 +2,7 @@
 
 export default function Menu({ categories, onSelectCategory }) {
     return (
-        <section className="md:w-[100%] h-[15px] rounded bg-white flex justify-center md:pt-16 pt-8 md:text-[16px] text-[15px] flex-wrap mb-[150px] mt-12 ">
+        <section className="md:w-[100%] h-[15px] rounded bg-white flex justify-center md:pt-16 pt-2 md:text-[16px] text-[15px] flex-wrap mb-[150px] mt-12 ">
 
             <ul className="flex gap-4 flex-wrap justify-center">
                 {categories.map((category) => (
