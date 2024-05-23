208bfa1f12fc926862072826c6b7206d0a2a9038
Kênia Caroline <keniacarolineferreira@gmail.com>
2024-05-22 22:17:40 -0300

    erro ajustado

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header(){
     
     return (
         <header className="bg-black w-full h-[100px] relative">
-            Apenas testando
+            
         </header>
 
     )
