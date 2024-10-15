cd7f91a600f1fc4380a562558e72706490f8c174
euukc <keniacarolineferreira@gmail.com>
2024-10-15 09:47:31 -0300

    adicionando infos na home

--- src/app/components/Apresentacao.jsx
+++ src/app/components/Apresentacao.jsx
@@ -47,7 +47,7 @@ export default function Apresentacao() {
 
 
   return (
-    <main className="md:w-[100%] sm:w-[100%] w-[98%] lg:w-[800px] rounded-lg bg-white relative mt-[-50px] p-[16px] flex flex-col mb-32 min-h-screen grow-1 shadow-2xl">
+    <main className="md:w-[100%] sm:w-[100%] w-[98%] lg:w-[1000px] rounded-lg bg-white relative mt-[-50px] p-[16px] flex flex-col mb-32 min-h-screen grow-1 shadow-2xl">
       <section className='flex w-full'>
         <div className='w-[30%] md:w-[20%]'>
           <Image className='w-[100%] rounded-lg' src="/logo-JR.png" alt='Logo do Mini Mercado JR' width={100} height={100}>
@@ -61,8 +61,9 @@ export default function Apresentacao() {
 
       <section className='pt-6 text-[13.5px] md:text-[16px] sm:text-[16px] '>
         <div> <OperatingStatus /></div>
-        <div> Pedido mínino para entrega: R$ 15,00 </div>
-        <div> </div>
+        <div> Horário de Funcionamento: 8h às 12h - 13h às 20:30 </div>
+        <div> Endereço: Rua Luis Alves Pereira, nº 322, Areal</div>
+        <div> Telefone: 53 98478-1410</div>
       </section>
 
       <Menu categories={categories} onSelectCategory={setSelectedCategory} />
